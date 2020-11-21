935f00c50833b4a294400112b3e498cd8559daeb
sketchysocks <58572875+sketchysocks@users.noreply.github.com>
2020-11-21 17:23:27 -0500

    Fix S2Cell Naming
    - Mistakes from merging

--- src/models/cell.js
+++ src/models/cell.js
@@ -9,7 +9,7 @@ const MySQLConnector = require('../services/mysql.js');
 const db = new MySQLConnector(config.db);
 
 /**
- * S2NCell model class.
+ * S2Cell model class.
  */
 class Cell {
 
@@ -25,30 +25,30 @@ class Cell {
         this.updated = updated;
     }
 
-    static getS2NCellIDs(minLevel, maxLevel, maxCells, polygon) {
+    static getS2CellIDs(minLevel, maxLevel, maxCells, polygon) {
         let bbox = turf.bbox(polygon);
-        let regionCoverer = new S2N.S2NRegionCoverer();
+        let regionCoverer = new S2N.S2RegionCoverer();
         regionCoverer.minLevel = minLevel;
         regionCoverer.maxLevel = maxLevel;
         regionCoverer.maxCells = maxCells;
-        let region = S2N.S2NLatLngRect.fromLatLng(
-            S2N.S2NLatLng.fromDegrees(bbox[1], bbox[0]),
-            S2N.S2NLatLng.fromDegrees(bbox[3], bbox[2])
+        let region = S2N.S2LatLngRect.fromLatLng(
+            S2N.S2LatLng.fromDegrees(bbox[1], bbox[0]),
+            S2N.S2LatLng.fromDegrees(bbox[3], bbox[2])
         );
         let cellIDsBBox = regionCoverer.getInteriorCoveringCells(region);
         let cellIDs = [];
         for (let i = 0; i < cellIDsBBox.length; i++) {
             let cellID = cellIDsBBox[i];
             let id = cellID.id.toUnsigned().toString();
-            let cell = new S2N.S2NCell(cellID);
+            let cell = new S2N.S2Cell(cellID);
             let vertex0 = cell.getVertex(0);
             let vertex1 = cell.getVertex(1);
             let vertex2 = cell.getVertex(2);
             let vertex3 = cell.getVertex(3);
-            let coord0 = S2N.S2NLatLng.fromPoint(new S2N.S2NPoint(vertex0.x, vertex0.y, vertex0.z));
-            let coord1 = S2N.S2NLatLng.fromPoint(new S2N.S2NPoint(vertex1.x, vertex1.y, vertex1.z));
-            let coord2 = S2N.S2NLatLng.fromPoint(new S2N.S2NPoint(vertex2.x, vertex2.y, vertex2.z));
-            let coord3 = S2N.S2NLatLng.fromPoint(new S2N.S2NPoint(vertex3.x, vertex3.y, vertex3.z));
+            let coord0 = S2N.S2LatLng.fromPoint(new S2N.S2Point(vertex0.x, vertex0.y, vertex0.z));
+            let coord1 = S2N.S2LatLng.fromPoint(new S2N.S2Point(vertex1.x, vertex1.y, vertex1.z));
+            let coord2 = S2N.S2LatLng.fromPoint(new S2N.S2Point(vertex2.x, vertex2.y, vertex2.z));
+            let coord3 = S2N.S2LatLng.fromPoint(new S2N.S2Point(vertex3.x, vertex3.y, vertex3.z));
             let point0 = turf.point([coord0.lngDegrees, coord0.latDegrees]);
             let point1 = turf.point([coord1.lngDegrees, coord1.latDegrees]);
             let point2 = turf.point([coord2.lngDegrees, coord2.latDegrees]);
