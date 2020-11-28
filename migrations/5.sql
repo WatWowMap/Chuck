eb41966ef33d1bacc746381d23402d6631b9a025
Mygod <contact-git@mygod.be>
2020-11-28 11:19:18 -0500

    Fix migration script

--- migrations/5.sql
+++ migrations/5.sql
@@ -1,5 +1,5 @@
 ALTER TABLE pokestop
-ADD COLUMN `ar_scan_eligible` tinyint(1) unsigned DEFAULT 0;
+ADD COLUMN `ar_scan_eligible` tinyint(1) unsigned;
 
 ALTER TABLE gym
-ADD COLUMN `ar_scan_eligible` tinyint(1) unsigned DEFAULT 0;
+ADD COLUMN `ar_scan_eligible` tinyint(1) unsigned;
