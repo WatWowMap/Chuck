98626d5eebeb5f99568f06cfbbf537c384dbec40
Furtif <msntchat@hotmail.fr>
2021-01-18 00:06:28 +0100

    :100: :100: :100: :100: +1

--- src/routes/routecontroller.js
+++ src/routes/routecontroller.js
@@ -73,7 +73,7 @@ class RouteController {
                     console.warn(`Ignoring non-raw message from ${username}`);
                     continue;
                 }
-                
+
                 // PD is sending more then we actually need.
                 // let's only care about certain protos
                 //if (![2, 5, 6, 101, 102, 104, 106, 156, 300, 5004, 5005].includes(parseInt(message['type']))) {
